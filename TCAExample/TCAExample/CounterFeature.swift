3ed98c31babc911cb8ace5b2653cb60c89f42f58
Aleksei Usanov <sunrizz@icloud.com>
2023-10-19 03:14:40 +0700

    add siide effect state&actions

--- TCAExample/TCAExample/CounterFeature.swift
+++ TCAExample/TCAExample/CounterFeature.swift
@@ -9,27 +9,34 @@ import ComposableArchitecture
 
 struct CounterFeature: Reducer {
     
-    struct State {
+    struct State: Equatable {
         var count = 0
+        var fact: String?
+        var isLoading = false
     }
     
     enum Action {
         case decrementButtonTapped
+        case factButtonTapped
         case incrementButtonTapped
     }
     
     func reduce(into state: inout State, action: Action) -> ComposableArchitecture.Effect<Action> {
         switch action {
         case .decrementButtonTapped:
+            state.fact = nil
             state.count -= 1
             return .none
         case .incrementButtonTapped:
+            state.fact = nil
             state.count += 1
+            return .none
+        case .factButtonTapped:
+            state.fact = nil
+            state.isLoading = true
             return .none
             
         }
     }
     
 }
-
-extension CounterFeature.State: Equatable {}
