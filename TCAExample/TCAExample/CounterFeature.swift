e43a55957ace68b373aa11c28f826d1b5477b591
Aleksei Usanov <sunrizz@icloud.com>
2023-10-18 13:51:23 +0700

    implement Reducer

--- TCAExample/TCAExample/CounterFeature.swift
+++ TCAExample/TCAExample/CounterFeature.swift
@@ -5,4 +5,29 @@
 //  Created by Sunrizz on 18.10.2023.
 //
 
-import Foundation
+import ComposableArchitecture
+
+struct CounterFeature: Reducer {
+    
+    struct State {
+        var count = 0
+    }
+    
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+    
+    func reduce(into state: inout State, action: Action) -> ComposableArchitecture.Effect<Action> {
+        switch action {
+        case .decrementButtonTapped:
+            state.count -= 1
+            return .none
+        case .incrementButtonTapped:
+            state.count += 1
+            return .none
+            
+        }
+    }
+    
+}
