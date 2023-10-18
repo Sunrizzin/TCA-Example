b69ec6d17daa94c59001268a036c24afc0407605
Aleksei Usanov <sunrizz@icloud.com>
2023-10-18 14:12:21 +0700

    change entry point

--- TCAExample/TCAExample/TCAExampleApp.swift
+++ TCAExample/TCAExample/TCAExampleApp.swift
@@ -5,13 +5,18 @@
 //  Created by Sunrizz on 18.10.2023.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCAExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(
+                store: Store(initialState: CounterFeature.State()) {
+                    CounterFeature()
+                }
+            )
         }
     }
 }
