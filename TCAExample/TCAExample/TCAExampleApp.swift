3cee5c350f22267068376ddb54e3909247f51f59
Aleksei Usanov <sunrizz@icloud.com>
2023-10-18 13:42:12 +0700

    first commit

---
+++ TCAExample/TCAExample/TCAExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAExampleApp.swift
+//  TCAExample
+//
+//  Created by Sunrizz on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
