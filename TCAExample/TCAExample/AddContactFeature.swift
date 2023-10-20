b5dc79dad3f7cd7113eb9940175c1c778d84c14b
Aleksei Usanov <sunrizz@icloud.com>
2023-10-20 22:46:08 +0700

    add delegate actions

--- TCAExample/TCAExample/AddContactFeature.swift
+++ TCAExample/TCAExample/AddContactFeature.swift
@@ -14,17 +14,31 @@ struct AddContactFeature: Reducer {
     }
     
     enum Action: Equatable {
+        case delegate(Delegate)
         case cancelButtonTapped
         case saveButtonTapped
         case setName(String)
+        enum Delegate: Equatable {
+            case saveContact(Contact)
+        }
     }
+    
+    @Dependency(\.dismiss) var dismiss
     
     func reduce(into state: inout State, action: Action) -> ComposableArchitecture.Effect<Action> {
         switch action {
         case .cancelButtonTapped:
+            return .run { _ in await self.dismiss() }
+            
+        case .delegate:
             return .none
+            
         case .saveButtonTapped:
-            return .none
+            return .run { [contact = state.contact] send in
+                await send(.delegate(.saveContact(contact)))
+                await self.dismiss()
+            }
+            
         case let .setName(name):
             state.contact.name = name
             return .none
