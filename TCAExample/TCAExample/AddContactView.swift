38f8716bc40703b3d79e40af9e1ab6f9049cc4bd
Aleksei Usanov <sunrizz@icloud.com>
2023-10-20 22:00:48 +0700

    add AddContactView

---
+++ TCAExample/TCAExample/AddContactView.swift
@@ -0,0 +1,48 @@
+//
+//  AddContactView.swift
+//  TCAExample
+//
+//  Created by Sunrizz on 20.10.2023.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct AddContactView: View {
+    var store: StoreOf<AddContactFeature>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            Form {
+                TextField("Name", text: viewStore.binding(get: \.contact.name, send: { .setName($0) }))
+                Button("Save") {
+                    viewStore.send(.saveButtonTapped)
+                }
+            }
+            .toolbar {
+                ToolbarItem {
+                    Button("Cancel") {
+                        viewStore.send(.cancelButtonTapped  )
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AddContactView(
+            store: Store(
+                initialState: AddContactFeature.State(
+                    contact: Contact(
+                        id: UUID(),
+                        name: "Blob"
+                    )
+                )
+            ) {
+                AddContactFeature()
+            }
+        )
+    }
+}
